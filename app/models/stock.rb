ab0b95621e5026d436eb99c60552838b70d4edf9
clementsm <clement@studiomelipone.eu>
2011-06-19 01:12:55 +0200

    add edit multiple to menu

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,9 +2,14 @@ class Stock < ActiveRecord::Base
   belongs_to :reference
   belongs_to :filiale
   
-  scope :accepted, :conditions => {:status => "valide"}
+  scope :accepted, where("status = 'valide'")
+  scope :alert, accepted.where("quantity <= min_alert")
   
   def reference_name
     return reference.name
+  end
+  
+  def alert?
+    quantity < min_alert?
   end
 end
