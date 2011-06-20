9b82f81b615e817e7ee16ab1e857d67bac912796
clementsm <clement@studiomelipone.eu>
2011-06-20 22:56:13 +0200

    fix nom

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
-  validates_presence_of :nom, :on => :create, :message => "can't be blank"
+  validates_presence_of :name, :on => :create, :message => "can't be blank"
   has_many :references
 end
